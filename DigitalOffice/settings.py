9fb852bab1de84617a4e2bdba901c3e1b3c89c12
Jalees-Jahanzaib <47352096+Jalees-Jahanzaib@users.noreply.github.com>
2020-01-06 06:46:58 +0530

    'ah'

--- DigitalOffice/settings.py
+++ DigitalOffice/settings.py
@@ -118,6 +118,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.11/howto/static-files/
 
 STATIC_URL = '/static/'
+MEDIA_ROOT=os.path.join(BASE_DIR,'staticfiles')
 
 CRISPY_TEMPLATE_PACK='bootstrap4'
 LOGIN_REDIRECT_URL='blog-home'
